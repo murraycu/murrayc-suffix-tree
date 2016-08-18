9f469b276943058a5de93a8ca17c7a5528d65f75
Murray Cumming <murrayc@murrayc.com>
2016-08-18 10:53:49 +0200

    whitespace fix

--- murrayc-suffix-tree/suffix_tree.h
+++ murrayc-suffix-tree/suffix_tree.h
@@ -353,7 +353,7 @@ private:
     if (!start_edge) {
       return result;
     }
-    
+ 
     const auto start_substr_used = start.substr_used_;
     if (start_substr_used != str_size(substr)) {
       return result;
